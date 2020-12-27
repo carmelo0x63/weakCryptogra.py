f3fb5d0e4f300bf6ea77a01abba77dc90abb2d53
Carmelo C <carmelo.califano@gmail.com>
2020-12-27 13:17:03 +0100

    First commit

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # Weak Cryptography
-They say, (deep voice) "Never roll your own cryptography!". And that's [right](https://www.schneier.com/blog/archives/2015/05/amateurs_produc.html)!
+They say, (deep voice) "Never roll your own cryptography!". And that's [right](https://www.schneier.com/blog/archives/2015/05/amateurs_produc.html)!</br>
 Except for one thing, how does one learn if (s)he's not allowed to make mistakes?
 
 `weakCryptogra.py` is a collection of **weak** ciphers, of debatable (if any) usefulness, whose role may be to quickly try some cryptography and cryptanalysis techniques.
 
-**Disclaimer**: the files contained herewith are clearly an implementation of **weak** cryptographic functions. Use them to try and learn as you please. **Never** use them for any serious, real-life applications!
+Heavily insipred by Al Sweigart's wonderful series of books, particularly [Cracking Codes with Python](https://inventwithpython.com/cracking/).
+
+**Disclaimer**: the files contained herewith are clearly an implementation of **weak** cryptographic functions.</br>
+Use them to try and learn as you please. **Never** use them for any serious, real-life applications!
+
+### Usage
+>>> import weakCryptogra
+
+>>> weakCryptogra.reverse('Hello, world!')
+'!dlrow ,olleH'
+
+>>> weakCryptogra.caesar('Hello, world!', 17)
+'YvCCF{ NFICu='
 
--- weakCryptogra.py
+++ weakCryptogra.py
@@ -11,6 +11,35 @@ def reverse(txt):
 
     return xtx
 
-def caesar(txt, key):
-    pass
+def caesar(txt, key, mode = 0):
+    """
+    The Caesar cipher works by substituting each letter of a message 'txt' with a new letter after shifting the alphabet over by 'key' positions. For example (e.g. key = 3), every A in the message would be replaced by a D, every B would be an E, and so on.
+    When a letter at the end of the alphabet must be shifted, such as Y, it must be wrapped around to the beginning of the alphabet and shifted three places to B.
+    'mode' takes two different values: 0 (default) = encrypt, 1 = decrypt.
+    """
+
+    SYMBOLS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&()*+,-./:;<=>?@[\]^_`{|}~'  # len(SYMBOLS) = 93
+    xtx = ''  # Output string
+
+    for letter in txt:
+        if letter in SYMBOLS:
+            letterIndex = SYMBOLS.find(letter)
+#            print(f'letter: {letter}\t index: {letterIndex}')
+
+            if mode:
+                newIndex = letterIndex - key  # if 'mode' is set the request is to decrypt, that is to shift left, hence the "-"
+            else:
+                newIndex = letterIndex + key  # if 'mode' is NOT set (default value) the request is to decrypt, letters are shifted right, hence the "+"
+
+            # Out of margins/wrap-around is handled here with some modulo-like logic
+            if newIndex >= len(SYMBOLS):
+                newIndex -= len(SYMBOLS)
+            elif newIndex < 0:
+                newIndex -= len(SYMBOLS)
+
+            xtx += SYMBOLS[newIndex]
+        else:
+            xtx += letter
+
+    return xtx
 
